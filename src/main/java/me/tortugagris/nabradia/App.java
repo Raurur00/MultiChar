6318f0e35821fe7444b81e8d4b7a86b05db5c70c
Javier Zavala <javierzavalatorres@gmail.com>
2021-05-20 17:01:06 -0400

    initial commit

---
+++ src/main/java/me/tortugagris/nabradia/App.java
@@ -0,0 +1,14 @@
+package me.tortugagris.nabradia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin
+{
+    public void onEnable(){
+        this.getLogger().info("Maven Work")
+    }
+}
